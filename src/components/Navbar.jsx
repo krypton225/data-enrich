a88f78d213025837f3914d73b5dcf52d4ddde8bf
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-12 09:44:09 +0200

    ADD HamburgerButton component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import NavbarItems from "../data/navbarItems";
 
+import { HamburgerButton } from "./index";
+
 import LogoImage from "../assets/logo.png";
 
 const Navbar = () => {
@@ -34,15 +36,7 @@ const Navbar = () => {
                     }
                 </ul>
 
-                <button className={`w-[1.8rem] h-[1rem] rounded-sm absolute right-5 top-1/2 -translate-y-1/2 z-[99999] md:hidden inline-flex flex-col justify-between items-center brightness-200 cursor-pointer ${mdScreen ? "rotate-[360deg]" : "rotate-0"} 
-                transition-all duration-[0.7s]`} onClick={changeBurgerIconHandler}>
-                    <span className={`w-full h-[2px] block bg-white ${mdScreen ? "rotate-45 translate-y-2" : "rotate-0"} 
-                    transition-all ease-out duration-[0.4s]`}></span>
-                    <span className={`w-full h-[2px] block bg-white ${mdScreen ? "opacity-0 translate-x-4" : "opacity-1"} 
-                    transition-all duration-[0.4s] ease-out delay-[0.4s]`}></span>
-                    <span className={`w-full h-[2px] block bg-white ${mdScreen ? "-rotate-45 -translate-y-3" : "rotate-0"} 
-                    transition-all ease-out`}></span>
-                </button>
+                <HamburgerButton onClickHandler={changeBurgerIconHandler} getMdScreenState={mdScreen} />
             </div>
 
             <ul className={`w-screen h-screen absolute top-0 ${mdScreen ? "left-0" : "-left-[200%]"} z-[9999] flex flex-col justify-center items-center md:hidden capitalize bg-primary transition-all duration-[0.6s] ease-in-out`}>
