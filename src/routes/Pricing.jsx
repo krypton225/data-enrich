50670ffff95853cc19cde8766006c2fe47087f0a
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-13 13:52:51 +0200

    ADD Pricing Route details

--- src/routes/Pricing.jsx
+++ src/routes/Pricing.jsx
@@ -1,7 +1,11 @@
+import { PricingComponent } from "../components";
+
 const Pricing = () => {
     return (
-        <div>
-            Pricing
+        <div className="w-full mt-7 mb-20 bg-white py-24">
+            <div className="container">
+                <PricingComponent />
+            </div>
         </div>
     )
 }
