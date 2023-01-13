58489974d428dae03765a86ad5d170023249d25f
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-13 13:50:41 +0200

    ADD Contact Route details

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,7 +1,11 @@
+import { ContactInfoComponent } from "../components";
+
 const Contact = () => {
     return (
-        <div>
-            Contact
+        <div className="bg-white">
+            <div className="container mb-20 pb-20 grid grid-cols-1 lg:grid-cols-2 gap-y-14 lg:gap-5 items-center">
+                <ContactInfoComponent />
+            </div>
         </div>
     )
 }
